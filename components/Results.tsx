92f9f9ced27f28b828599b7a0be12f9617e7bcf5
uditsingh007 <singh.udit14@gmail.com>
2022-07-22 21:53:03 +0530

    build fixed

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,6 +1,25 @@
 import React from "react";
 import Thumbnail from "./Thumbnail";
-import { Movie } from "../../typings";
+
+export interface Movie {
+  title: string;
+  backdrop_path: string;
+  media_type?: string;
+  release_date?: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+  adult: boolean;
+  video: boolean;
+  original_title: string;
+  name: string;
+  first_air_date: string;
+}
 
 const Results = ({ results }: { results: Movie[] }) => {
   return (
